d0da77daf7fe6f775d31234b6674c7e5cd401279
nikmk <41271779+nikmk@users.noreply.github.com>
2020-08-13 17:50:31 +0530

    Umm

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -43,7 +44,69 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
 
-      body: ListView()
+      body: ListView(
+        shrinkWrap: true ,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                alignment: Alignment(0.0, -0.4),
+                height:100.0,
+                color: Colors.white,
+                child: Text(
+                  "Get coaching",
+                  style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.w600),
+                )
+              ),
+              Container(
+                margin:EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
+                decoration:BoxDecoration(
+                  color: Colors.white ,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                    blurRadius: 2.0,
+                      color: Colors.grey
+                  )]
+                ),
+                child:Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:<Widget>[
+                    Stack(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(25.0, 25.0, 5.0, 5.0),
+                        child: Text(
+                          "You have",
+                          style: TextStyle(
+                            color:Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+
+                          )
+                        )
+                      ),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(25.0, 40.0, 5.0, 5.0),
+                          child: Text(
+                              "206",
+                              style: TextStyle(
+                                color:Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40.0,
+
+                              )
+                          )
+                      )
+                    ]
+                    )
+                  ]
+                )
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 }
